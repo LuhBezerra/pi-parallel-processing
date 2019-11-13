50deb6c1b3a29e5210024c32ccaa75fb999be277
Samuel Veloso <samuelveloso42@gmail.com>
2019-11-13 19:49:50 -0300

    Deletion comments

--- client.c
+++ client.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-//socket
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
--- server.c
+++ server.c
@@ -1,5 +1,3 @@
-//  gcc server.c -lpthread -o server
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
